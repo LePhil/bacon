ec4811b2eae8e261e07acc805ea8378b9a89b488
LePhil <philipp.christen@gmail.com>
2013-12-15 22:40:30 +0100

    translations-module

---
+++ public/translations.js
@@ -0,0 +1,18 @@
+define('translations', [], function () {
+	return {
+		"en-US": {
+			"login": "Login",
+			"login-failed": "Login failed",
+			"register-success": "Registration successful!",
+			"register-failed": "Registration failed! User exists or didn't provide username and password",
+			"username-and-password-empty": "Username and password cannot be empty"
+		},
+		"de-DE": {
+			"login": "Einloggen",
+			"login-failed": "Login fehlgeschlagen",
+			"register-success": "Registrierung erfolgreich!",
+			"register-failed": "Registrierung fehlgeschlagen. User existiert bereits oder nicht alle Angaben erfüllt.",
+			"username-and-password-empty": "Username und Passwort können nicht leer bleiben"
+		}
+	};
+});
